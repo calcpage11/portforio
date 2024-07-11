acacefdc0638a0e5bea02e1b4a47867dbc63ffdf
calcpage11 <154405359+calcpage11@users.noreply.github.com>
2024-07-12 08:17:58 +0900

    Add files via upload

---
+++ 240712_portforio/js/script.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper(".header-slider-swiper" , {
+
+  pagination: {
+    el: ".header-slider-pagination"
+  },
+
+  navigation: {
+    nextEl: ".header-slider-button-next",
+    prevEl: ".header-slider-button-prev"
+  }
+
+});
+
